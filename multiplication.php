62a116412b7218872b2cd75128087aae7d4afdc7
gcerni <cerniglia.gianluca@gmail.com>
2017-03-20 00:47:41 +0000

    final up

--- multiplication.php
+++ multiplication.php
@@ -8,26 +8,26 @@
 <body>
 	<table>
 		<?php 
-		//create first row of table headers 
+			//create first row of table headers 
 			echo '<tr>';
-		echo '<th>&nbsp;</th>';
-		for ($number = 1; $number <=12; $number++) :
-			echo "<th> $number <br> </th>" ;
-		endfor;
-		echo '</tr>';
-		// create remaining rows 
-		for ($row = 1, $number = 1; $row <= 12; $row++) :
-			echo '<tr>';
-		//first cell is a table header 
-			if ($number == 1) {
-				echo "<th> $row </th>";
-			}
-			while ($number <= 12 ) :
-				echo '<td>' . $row * $number++ . '</td>';
-			endwhile;
+				echo '<th>&nbsp;</th>';
+				for ($number = 1; $number <=12; $number++) :
+					echo "<th> $number <br> </th>" ;
+				endfor;
 			echo '</tr>';
-			$number = 1;
-		endfor;
+			// create remaining rows 
+			for ($row = 1, $number = 1; $row <= 12; $row++) :
+				echo '<tr>';
+				//first cell is a table header 
+					if ($number == 1) {
+						echo "<th> $row </th>";
+					}
+					while ($number <= 12 ) :
+						echo '<td>' . $row * $number++ . '</td>';
+					endwhile;
+				echo '</tr>';
+				$number = 1;
+			endfor;
 		?>
 	</table>
 </body>
