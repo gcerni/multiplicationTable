51e1a66f163e3cdfcb15ac3d797e234deceb87b6
gcerni <cerniglia.gianluca@gmail.com>
2017-03-20 00:20:25 +0000

    up to date

--- multiplication.php
+++ multiplication.php
@@ -7,29 +7,29 @@
 <body>
 <table>
 	<?php 
-	//create first row of table headers 
-	echo '<tr>';
-	echo '<th>&nbsp;</th>';
-for ($number = 1; $number <=12; $number++) :
-	echo "<th> $number <br> </th>" ;
-endfor;
-echo '</tr>';
+		//create first row of table headers 
+		echo '<tr>';
+		echo '<th>&nbsp;</th>';
+			for ($number = 1; $number <=12; $number++) :
+				echo "<th> $number <br> </th>" ;
+			endfor;
+				echo '</tr>';
 
-// create remaining rows 
-for ($row = 1, $number = 1; $row <= 12; $row++) :
-	echo '<tr>';
-	//first cell is a table header 
-	if ($number == 1) {
-		echo "<th> $row </th>";
-	}
+		// create remaining rows 
+			for ($row = 1, $number = 1; $row <= 12; $row++) :
+				echo '<tr>';
+		//first cell is a table header 
+			if ($number == 1) {
+				echo "<th> $row </th>";
+			}
 
-	while ($number <= 12 ) :
-	echo '<td>' . $row * $number++ . '</td>';
-	endwhile;
-	echo '</tr>';
-	$number = 1;
-endfor;
-?>
+			while ($number <= 12 ) :
+				echo '<td>' . $row * $number++ . '</td>';
+			endwhile;
+				echo '</tr>';
+				$number = 1;
+			endfor;
+	?>
 
 </table>
 </body>
