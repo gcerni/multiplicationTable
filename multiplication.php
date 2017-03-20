6a436cd3cf65b2c866de608474fa9a33b35bc9fc
gcerni <cerniglia.gianluca@gmail.com>
2017-03-20 00:14:55 +0000

    first up

---
+++ multiplication.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Multiplication table</title>
+	<link href="styles.css" rel="stylesheet" type="text/css">
+<body>
+<table>
+	<?php 
+	//create first row of table headers 
+	echo '<tr>';
+	echo '<th>&nbsp;</th>';
+for ($number = 1; $number <=12; $number++) :
+	echo "<th> $number <br> </th>" ;
+endfor;
+echo '</tr>';
+
+// create remaining rows 
+for ($row = 1, $number = 1; $row <= 12; $row++) :
+	echo '<tr>';
+	//first cell is a table header 
+	if ($number == 1) {
+		echo "<th> $row </th>";
+	}
+
+	while ($number <= 12 ) :
+	echo '<td>' . $row * $number++ . '</td>';
+	endwhile;
+	echo '</tr>';
+	$number = 1;
+endfor;
+?>
+
+</table>
+</body>
+</html>
