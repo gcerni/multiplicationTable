8716b198b878820b996f0223a32806f88e01e8e6
gcerni <cerniglia.gianluca@gmail.com>
2017-03-21 18:14:24 +0000

    Indentation corrected

--- Multipli.php
+++ Multipli.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Multiplication Table</title>
-</head>
-<body>
-<table>
-	<?php 
-		//create first row of table headers
-		echo '<tr>';
-			echo '<th>&nbsp;</th>';
-			for ($number = 1; $number <= 12; $number++):
-					echo "<th> $number <br> </th>";
-			endfor;
-		echo '</tr>';
-		// create remaining rows 
-		for ($row = 1, $number = 1; $row <= 12; $row++) :
+	<head>
+		<title>Multiplication table</title>
+	</head>
+	<body>
+		<table>
+			<?php
+			//create first row of table headers
 			echo '<tr>';
+			echo '<th>&nbsp;</th>';
+			for ($number = 1; $number <= 12; $number++) :
+				echo "<th> $number <br> </th>";
+			endfor;
+			echo '</tr>';
+			// create remaining rows
+			for ($row = 1, $number = 1; $row <= 12; $row++) :
+				echo '<tr>';
 				//first cell is a table header
 				if ($number == 1) {
 					echo "<th> $row </th>";
@@ -23,10 +23,10 @@
 				while ($number <= 12) :
 					echo '<td>' . $row * $number++ . '</td>';
 				endwhile;
-			echo '</tr>';
-			$number = 1;
-		endfor;
+				echo '</tr>';
+				$number = 1;
+			endfor;
 	?>
 </table>
-</body>
+	</body>
 </html>
